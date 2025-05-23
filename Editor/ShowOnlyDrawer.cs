dede050ff951c2cbe7ee2c81e2c867b384e025ea
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-23 10:43:07 +0200

    ⠀⠀⠀⠀⠀

--- Editor/ShowOnlyDrawer.cs
+++ Editor/ShowOnlyDrawer.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Provides a custom property drawer for fields marked with the <see cref="ShowOnlyAttribute"/>.
+    /// </summary>
+    /// <remarks>This drawer renders the field as a read-only label in the Unity Inspector, displaying the
+    /// field's name and value. It is intended for use with fields that should be visible in the Inspector but not
+    /// editable by the user.</remarks>
     [CustomPropertyDrawer(typeof(ShowOnlyAttribute))]
     public class ShowOnlyDrawer : PropertyDrawer
     {
+        /// <summary>
+        /// Renders the custom GUI for a serialized property in the Unity Inspector.
+        /// </summary>
+        /// <remarks>This method overrides the default behavior to display the property's name and value
+        /// in a custom layout. The property's value is retrieved and displayed as a string.</remarks>
+        /// <param name="position">The rectangle on the screen to use for the property GUI.</param>
+        /// <param name="property">The serialized property to display and edit.</param>
+        /// <param name="label">The label to display alongside the property in the Inspector.</param>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.LabelField(position, property.displayName);
@@ -20,6 +34,12 @@ namespace UnityEssentials
             EditorGUI.LabelField(valuePosition, valueString, EditorStyles.wordWrappedLabel);
         }
 
+        /// <summary>
+        /// Calculates the height required to display the content of a serialized property in the inspector.
+        /// </summary>
+        /// <param name="property">The serialized property whose content height is being calculated.</param>
+        /// <param name="label">The label associated with the property, typically displayed in the inspector.</param>
+        /// <returns>The height, in pixels, required to render the property's content with word wrapping applied.</returns>
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             float indent = EditorGUI.indentLevel * 16;
--- Runtime/ShowOnlyAttribute.cs
+++ Runtime/ShowOnlyAttribute.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Specifies that the associated property should only be displayed in the user interface without allowing edits.
+    /// </summary>
     public class ShowOnlyAttribute : PropertyAttribute { }
 }
