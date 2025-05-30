3e53b728fcf406a6bc5cae7b8bb0ce0f69942a76
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-30 15:57:45 +0200

    ⠀⠀⠀⠀⠀

--- Editor/ReadOnlyDrawer.cs
+++ Editor/ReadOnlyDrawer.cs
@@ -13,16 +13,21 @@ namespace UnityEssentials
     [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
     public class ReadOnlyDrawer : PropertyDrawer
     {
-        /// <summary>
-        /// Renders the custom GUI for a serialized property in the Unity Inspector.
-        /// </summary>
-        /// <remarks>This method overrides the default behavior to display the property's name and value
-        /// in a custom layout. The property's value is retrieved and displayed as a string.</remarks>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.LabelField(position, property.displayName);
 
-            var valueString = InspectorHookUtilities.GetPropertyValue(property).ToString();
+            string valueString;
+            if (property.propertyType == SerializedPropertyType.Enum)
+            {
+                int index = property.enumValueIndex;
+                string enumName = (index >= 0 && index < property.enumDisplayNames.Length)
+                    ? property.enumDisplayNames[index]
+                    : "Unknown";
+                valueString = $"{enumName} ({index})";
+            }
+            else valueString = InspectorHookUtilities.GetPropertyValue(property)?.ToString();
+
             var valuePosition = new Rect(position)
             {
                 x = string.IsNullOrEmpty(property.displayName) ? 16 : 20 + EditorGUIUtility.labelWidth,
@@ -31,12 +36,6 @@ namespace UnityEssentials
             EditorGUI.LabelField(valuePosition, valueString, EditorStyles.wordWrappedLabel);
         }
 
-        /// <summary>
-        /// Calculates the height required to display the content of a serialized property in the inspector.
-        /// </summary>
-        /// <param name="property">The serialized property whose content height is being calculated.</param>
-        /// <param name="label">The label associated with the property, typically displayed in the inspector.</param>
-        /// <returns>The height, in pixels, required to render the property's content with word wrapping applied.</returns>
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             float indent = EditorGUI.indentLevel * 16;
